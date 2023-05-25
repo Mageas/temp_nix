2d7cf18b45b0e202c97f9d41a215d344431ec84a
Mageas <gpg@mageas.net>
2023-05-25 09:39:55 +0200

    update

--- hosts/kuro/default.nix
+++ hosts/kuro/default.nix
@@ -4,27 +4,34 @@
     ../home.nix
     ./hardware-configuration.nix
   ];
+  
+  services.xserver.enable = true; # CUSTOMIZED
+  services.xserver.layout = "us"; # CUSTOMIZED
+  services.xserver.libinput.enable = true; # CUSTOMIZED
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm.wayland = false;
+  services.xserver.desktopManager.gnome3.enable = true;
 
   ## Modules
-  modules = {
-    desktop = {
-      bspwm.enable = true;
-      apps = {
-        rofi.enable = true;
-     };
-      browsers = {
-        default = "brave";
-        brave.enable = true;
-     };
-     term = {
-        default = "xterm";
-     };
-   };
-   editors = {
-      default = "nvim";
-      vim.enable = true;
-    };
-  };
+  # modules = {
+  #   desktop = {
+  #     bspwm.enable = true;
+  #     apps = {
+  #       rofi.enable = true;
+  #    };
+  #     browsers = {
+  #       default = "brave";
+  #       brave.enable = true;
+  #    };
+  #    term = {
+  #       default = "xterm";
+  #    };
+  #  };
+  #  editors = {
+  #     default = "nvim";
+  #     vim.enable = true;
+  #   };
+  # };
 
 
   ## Local config
