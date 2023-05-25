e16eec0fac1c23609030abb522a505bbd7b7f0a7
Mageas <gpg@mageas.net>
2023-05-25 09:41:14 +0200

    update

--- hosts/kuro/default.nix
+++ hosts/kuro/default.nix
@@ -10,7 +10,7 @@
   services.xserver.libinput.enable = true; # CUSTOMIZED
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.displayManager.gdm.wayland = false;
-  services.xserver.desktopManager.gnome3.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
   ## Modules
   # modules = {
