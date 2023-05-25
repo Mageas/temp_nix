958f47e57c6246c81999226a5fa6cdf69f449cef
Mageas <gpg@mageas.net>
2023-05-25 09:50:09 +0200

    update

--- hosts/kuro/install_nix.sh
+++ hosts/kuro/install_nix.sh
@@ -9,14 +9,14 @@ sudo git clone https://github.com/mageas/temp_nix --branch clean /etc/dotfiles ;
     cd /etc/dotfiles
 
 # Create a host config in `hosts/` and add it to the repo:
-sudo mkdir -p hosts/$HOST ;
-    sudo nixos-generate-config --root /mnt --dir /etc/dotfiles/hosts/$HOST ;
-    sudo rm -f hosts/$HOST/configuration.nix ;
-    sudo cp hosts/kuro/default.nix hosts/$HOST/default.nix ;
-    sudo git add hosts/$HOST
+sudo mkdir -p hosts/nixos ;
+    sudo nixos-generate-config --root /mnt --dir /etc/dotfiles/hosts/nixos ;
+    sudo rm -f hosts/nixos/configuration.nix ;
+    sudo cp hosts/kuro/default.nix hosts/nixos/default.nix ;
+    sudo git add hosts/nixos
 
 # Install nixOS
-USER=$USER sudo nixos-install --root /mnt --impure --flake .#$HOST
+sudo USER=nix nixos-install --root /mnt --impure --flake .#nixos
 
 # If you get 'unrecognized option: --impure', replace '--impure' with 
 # `--option pure-eval no`.
