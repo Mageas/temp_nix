d3d5a7d6f2a63dc2435caeb11337cbe4f8d9995f
Mageas <gpg@mageas.net>
2023-05-25 08:26:59 +0200

    update

--- hosts/home.nix
+++ hosts/home.nix
@@ -5,4 +5,13 @@ with lib;
 {
   time.timeZone = mkDefault "Europe/Paris";
   i18n.defaultLocale = mkDefault "en_US.UTF-8";
+  
+  # For redshift, mainly
+  location = (if config.time.timeZone == "America/Toronto" then {
+    latitude = 43.70011;
+    longitude = -79.4163;
+  } else if config.time.timeZone == "Europe/Copenhagen" then {
+    latitude = 55.88;
+    longitude = 12.5;
+  } else {});
 }
